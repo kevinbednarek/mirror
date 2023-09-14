e010279f789e7b6f64b9f3ff81d650a4dab60bef
kevinbednarek <60374582+kevinbednarek@users.noreply.github.com>
2023-09-14 14:51:02 -0500

    Init commit

---
+++ app.py
@@ -0,0 +1,33 @@
+from flask import Flask, render_template
+from mouse import get_mouse
+from handrecognition import hand_stuff
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+@app.route('/kevin')
+def kevin():
+    return render_template('kevin.html')
+
+
+@app.route('/background_process_test')
+def background_process_test():
+    print("Let's do hand stuff")
+    get_mouse()
+    return "nothing"
+
+
+# For Raspberry PI
+# if __name__ == '__main__':
+#     app.run(debug=True, host='0.0.0.0')
+
+# For development
+if __name__ == '__main__':
+    hand_stuff()
+    # get_mouse()
+    # app.run(debug=True, port=8001)
